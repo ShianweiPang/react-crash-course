704f140af29dc400738e400ff5ceec3090d2b3b6
Shian_wei_Pang <Shian_wei.Pang@Dell.com>
2023-02-24 18:17:33 +0800

    created components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import Todo from './components/Todo';
+
 function App() {
-  return <div>Hello!</div>;
+  return (
+    <div>
+      <h1>My todos</h1>
+      <Todo text="learn react 1"/>
+      <Todo text="learn react 2"/>
+      <Todo text="learn react 3"/>
+      <Todo text="learn react 4"/>
+    </div>
+  );
 }
 
 export default App;
